b6e07231bf050515dcaf0c63fcaeb157952a7b0a
TunahanDemirtunc <tunahandemirtunc07@gmail.com>
2023-08-02 06:03:09 +0300

    Initial commit

---
+++ CoreMe/Controllers/Category.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using CoreMe.Models;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMe.Controllers
+{
+    public class Category : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var values=cm.GetList();    
+            return View(values);
+        }
+    }
+}
